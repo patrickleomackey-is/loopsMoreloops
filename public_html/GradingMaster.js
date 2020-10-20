f482b6ebe45b71bc7fd00bd2fb3c81f295b60f79
MilleKel <MilleKel@TS31528.NIACC.EDU>
2020-10-19 21:18:14 -0500

    Initial commit

---
+++ public_html/GradingMaster.js
@@ -0,0 +1,16 @@
+/* 
+ * initialize an arry with a set of 5 test scores
+ * calculate the average score
+ * display the grade according to the following scale:
+ *      90+ = A
+ *      80-89 = B
+ *      70-79 = C
+ *      60-69 = D
+ *      <60 = F
+ *      
+ * BONUS: Get scores from the user instead of initializing them in the program
+ */
+
+document.write("<p>Grade calculation program</p>");
+
+
