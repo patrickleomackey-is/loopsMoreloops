55cd75768699ae3bb3b15f76d804d747097cdbb7
patrickleomackey-is <plmackey@my.niacc.edu>
2020-10-22 17:19:21 -0500

    This is it..loops 2...the grading assignment.

--- public_html/GradingMaster.js
+++ public_html/GradingMaster.js
@@ -7,10 +7,30 @@
  *      70-79 = C
  *      60-69 = D
  *      <60 = F
- *      
+ *   ??   
  * BONUS: Get scores from the user instead of initializing them in the program
  */
 
 document.write("<p>Grade calculation program</p>");
+let grade = [];
+let numberOftests = Number(prompt("Enter the number of tests to grade"));
+for (i = 0;i < numberOftests; i++){
+    grade.push(Number(prompt("Enter the test score")));
+    if (grade[i] >= 90)
+                       {document.write("<br>A");}
+    
+    if ((grade[i] >= 80)&&(grade[i]<= 89))
+                                          {document.write("<br>B");}
+    
+    if ((grade[i] >= 70)&&(grade[i] <= 79))
+                                            {document.write("<br>C");}
+    if ((grade[i] >= 60)&&(grade[i] <= 69))
+                                           {document.write("<br>D");}
+    if (grade[i] < 60)
+                      {document.write('<br>F');}                                       
+        
+}
+let total = 0;
+for(var i in grade){total += grade[i];}
 
-
+document.write("<br>"+ (total/ grade.length)+"  is the average test score");
